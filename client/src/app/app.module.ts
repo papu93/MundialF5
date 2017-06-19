ff2f4391c51355a22d8e056fe39c01ab171510c5
chiri <chirichignoa@gmail.com>
2017-06-19 19:02:52 -0300

    Cambios en app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,19 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { NationalTeamsService } from './services/national-teams.service';
+import { PlayersService } from './services/players.service';
 
+// Routes
+import { APP_ROUTING } from './app.routes';
+
+// Services
+
+// Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { NationalTeamsComponent } from './components/national-teams/national-teams.component';
+import { PlayersComponent } from './components/players/players.component';
+import { NationalTeamComponent } from './components/national-team/national-team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    NationalTeamsComponent,
+    PlayersComponent,
+    NationalTeamComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    NationalTeamsService,
+    PlayersService
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
