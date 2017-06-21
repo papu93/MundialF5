c7bf249a037abb8ead8e9b9699a26b7d8e78137c
chiri <chirichignoa@gmail.com>
2017-06-21 18:20:43 -0300

    Servicios

--- client/src/app/services/players.service.ts
+++ client/src/app/services/players.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class PlayersService {
 
-  constructor() { }
+  players: any[]= [];
+  apiURL = 'http://localhost:3977/api';
+
+  constructor( private httpModule: Http ) { }
+
+  getPlayers(nationalTeamId: number) {
+    let query;
+    if (nationalTeamId == null) {
+      query = `/getPlayers/`;
+    } else {
+      query = `/getPlayers/` + nationalTeamId;
+    }
+    const url = this.apiURL + query;
+    return this.httpModule.get( url )
+      .map( res => {
+        console.log(res.json().players);
+      });
+  }
 
 }
