8c253efd28a9b6b05b334ec92d2ac968fd04882b
Damian Dominguez <damian93domin@gmail.com>
2017-07-05 16:52:45 -0300

    getPlayers en el componente Players

--- client/src/app/components/players/players.component.ts
+++ client/src/app/components/players/players.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import { PlayersService } from '../../services/players.service';
+import {Player} from '../../classes/player.class';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-players',
@@ -7,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayersComponent implements OnInit {
 
-  constructor() { }
+  players: Player[] = [];
+  p: Player = new Player();
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private playersService: PlayersService) { }
 
   ngOnInit() {
+    this.getPlayers();
   }
 
+  getPlayers(){
+    this.activatedRoute.params
+      .switchMap((params: Params) => this.playersService.getPlayers(null))
+      .subscribe(
+        response => {
+          if(!response.players){
+            console.log("No hay jugadores para cargar");
+          }else{
+            this.players = response.players;
+            console.log(this.players);
+          }
+        },
+        error => {
+          var errorMessage = <any>error;
+          if(errorMessage != null){
+            console.log(JSON.parse(error._body));
+          }
+        });
+  }
 }
